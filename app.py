39ca92812ad59898d247ce6c6ab5239bdfe65db6
meenumeenal16 <meenumeenal16@gmail.com>
2024-03-04 13:49:53 +0530

    new change

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-print("sample program")
-print("sample program 2")
-print("sample program 3")
-print("sample program 4")
-print("sample program 6")
-
-a = 10
-b = 4
-c = a + b
+print("statement 1*")
+print("statement 2*")
+print("statement 3*")
+      
